a2a48d6ace227b34d307ebdbce89d3150fe25d41
sasha <apushkin20190310@icloud.com>
2025-03-16 17:25:05 +0300

    Доработка. Сообщение об исключении

--- headers/main.h
+++ headers/main.h
@@ -11,6 +11,9 @@
 #include "sdltile.h"
 #include "tiletools.h"
 
+#define exit_on_sdl_error() { printf("%s:%u: \"%s\"\n", __FILE__, __LINE__, SDL_GetError()); cpptrace::from_current_exception().print(); throw std::runtime_error(""); }
+#define warn_on_sdl_error() { printf("внимание: %s:%u: \"%s\"\n", __FILE__, __LINE__, SDL_GetError()); }
+
 /// @brief Обработчик событий SDL
 /// @param userdata Указатель на произвольные данные
 /// @param event Событие 
