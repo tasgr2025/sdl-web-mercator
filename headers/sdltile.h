d2206bea02a5f322684592dc6c330122a03ba17c
sasha <apushkin20190310@icloud.com>
2025-03-29 19:50:35 +0300

    Исправление. Здесь должен быть тип из SDL (?)

--- headers/sdltile.h
+++ headers/sdltile.h
@@ -6,13 +6,13 @@
 
 
 class SDLTile {
-    uint32_t i{0U}, x{0U}, y{0U}, z{0U}, t{0U};
+    Uint32 x{0U}, y{0U}, z{0U}, t{0U};
     SDL_Texture* texture{nullptr};
     Uint32 tick{0U};
 public:
     SDLTile() {}
     ~SDLTile () { if (texture) SDL_DestroyTexture(texture); }
-    SDLTile(uint32_t i0, uint32_t x0, uint32_t y0, uint32_t z0): i{i0}, x{x0}, y{y0}, z{z0} {}
+    SDLTile(Uint32 x0, Uint32 y0, Uint32 z0): x{x0}, y{y0}, z{z0} {}
     std::string get_url(const std::string& base_url) { return std::vformat(base_url, std::make_format_args(x, y, z)); }
     bool set_texture_from_data(SDL_Renderer *render, const char *data, const size_t len);
     SDL_Texture* get_texture() { return texture; }
